433aeaf6e2aa92db5c51611d3698bd6805a46b72
black-RAM <pplanes311@gmail.com>
2026-04-26 18:01:10 +0300

    move generated files out of src so linter doesn't see them

--- api/src/plugins/prisma.ts
+++ api/src/plugins/prisma.ts
@@ -1,6 +1,6 @@
 import fp from 'fastify-plugin';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../prisma/client.js';
+import { PrismaClient } from '../../prisma/generated/client.js';
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
