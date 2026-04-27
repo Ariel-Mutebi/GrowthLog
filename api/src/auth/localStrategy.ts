27fae14c6cbc9c4141534106d31f9279b6f3563b
black-RAM <pplanes311@gmail.com>
2026-04-27 16:54:28 +0300

    timing attack protection

--- api/src/auth/localStrategy.ts
+++ api/src/auth/localStrategy.ts
@@ -2,28 +2,25 @@ import { PrismaClient } from '../../prisma/generated/client.js';
 import { Strategy } from 'passport-local';
 import { compare } from 'bcrypt';
 
+const DUMMY_HASH = '$2a$12$R9h/cIPz0gi.URNNX3kh2OPST9/PgBkqquzi.Ss7KIUgO2t0jWMUW'; // prevent timing attack
+
 export function buildLocalStrategy(prisma: PrismaClient) {
-  return new Strategy(async (username, password, done) => {
-    try {
-      const user = await prisma.user.findUnique({
-        where: {
-          username,
-        },
-      });
+  return new Strategy(
+    {
+      usernameField: 'email',
+    },
+    async (email, password, done) => {
+      try {
+        const user = await prisma.user.findUnique({where: { email } });
+        const passwordsMatch = await compare(password, user?.password ?? DUMMY_HASH);
 
-      if (!user) {
-        return done(null, false, { message: 'Invalid username or password' });
+        if (!user || !passwordsMatch) {
+          return done(null, false, { message: 'Invalid email or password' });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error);
       }
-
-      const passwordsMatch = await compare(password, user.password);
-
-      if (!passwordsMatch) {
-        return done(null, false, { message: 'Invalid username or password' });
-      }
-
-      return done(null, user);
-    } catch (error) {
-      done(error);
-    }
-  });
+    });
 }
