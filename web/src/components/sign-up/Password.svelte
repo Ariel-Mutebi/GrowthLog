f4847bce87099b3e1cb05a6b29893da318292620
black-RAM <pplanes311@gmail.com>
2026-08-11 20:33:21 +0300

    feat: implement light mode for sign-up form

--- web/src/components/sign-up/Password.svelte
+++ web/src/components/sign-up/Password.svelte
@@ -47,18 +47,18 @@
   {/snippet}
 
   {#snippet underInput()}
-    <ul class="flex gap-3 h-2" aria-label={`Password strength (0 - 4): ${score}`}>
+    <ul class="flex gap-3 h-2" aria-label="Password strength (0 - 4): {score}">
       {#each fills as fill}
-        <li class="relative border border-neutral-600 grow rounded-sm bg-[#E3D2EF80] overflow-hidden">
+        <li class="bg-[#ADCAD8BF] dark:bg-[#E3D2EF80] relative border border-neutral-600 grow rounded-sm overflow-hidden">
           <div
             style="width: {fill.current * 100}%"
-            class="absolute inset-y-0 left-0 bg-cyan-600 rounded-sm"
+            class="absolute inset-y-0 left-0 bg-teal-400 dark:bg-cyan-600 rounded-sm"
           >
             <svg
               viewBox="-10 0 20 20"
               preserveAspectRatio="none"
               style="opacity: {Math.min(fill.current * 10, 1)}"
-              class="absolute inset-y-0 left-full w-4 translate-x-[-55%] text-cyan-600"
+              class="absolute inset-y-0 left-full w-4 translate-x-[-55%] text-teal-400 dark:text-cyan-600"
             >
               <path d="M0,0 C8,2 8,8 0,10 C-8,12 -8,18 0,20 Z" fill="currentColor" />
             </svg>
--- web/src/components/sign-up/Submit.svelte
+++ web/src/components/sign-up/Submit.svelte
@@ -2,10 +2,7 @@
   import { Tween } from 'svelte/motion';
   import { cubicOut } from 'svelte/easing';
 
-  const pos = new Tween(
-    { x: 90, y: 50 },
-    { duration: 300, easing: cubicOut }
-  );
+  const pos = new Tween({ x: 90, y: 50 }, { duration: 300, easing: cubicOut });
 
   function updatePosition(e: PointerEvent & { currentTarget: HTMLButtonElement }) {
     const rect = e.currentTarget.getBoundingClientRect();
@@ -22,8 +19,13 @@
 
 <button
   type="submit"
-  class="p-2 rounded-lg text-white text-xl"
-  style="background-image: radial-gradient(circle at {pos.current.x}% {pos.current.y}%, #484848, #e3d2ef80);"
+  class="
+    p-2 rounded-lg text-white text-xl font-semibold
+    [--gradient-start:#72BEB199] [--gradient-end:#C9D0C1CC]
+    dark:[--gradient-start:#484848] dark:[--gradient-end:#e3d2ef80]"
+  style="background-image: radial-gradient(
+    circle at {pos.current.x}% {pos.current.y}%,
+    var(--gradient-start), var(--gradient-end));"
   onpointermove={updatePosition}
   onpointerleave={resetPosition}
 >
